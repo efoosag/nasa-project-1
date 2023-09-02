6fcb987cd6962bfeb081d14201974b8822fd664c
Victor E. Osagie <efoosagsite@gmail.com>
2023-09-02 04:21:28 +0100

    trying es6

--- server/src/model/planets.model.js
+++ server/src/model/planets.model.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const { parse } = require('csv-parse');
+import { parse } from 'csv-parse';
 
 const planets = require('./planets.mongo');
 
